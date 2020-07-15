eed6aa7829eb87f0cd2f47eac3ce9bedd5ce75c3
Zhang <jamesz@ri-net.com>
2020-07-15 13:21:45 -0700

    taskbox UI

---
+++ storybook-static/precache-manifest.b45d37e401ddb4ea3355bd6c832ca283.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18ed63ab4e88c62f1b8d9f11b9ff1ff4",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.b577d0ea3e1da76bf543.bundle.js"
+  },
+  {
+    "url": "runtime~main.b577d0ea3e1da76bf543.bundle.js"
+  },
+  {
+    "revision": "559db64b935852b4a815",
+    "url": "static/css/main.b988a73d.chunk.css"
+  },
+  {
+    "url": "vendors~main.b577d0ea3e1da76bf543.bundle.js"
+  },
+  {
+    "url": "vendors~main.b577d0ea3e1da76bf543.bundle.js.LICENSE.txt"
+  }
+]);
